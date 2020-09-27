4abd5bf17d2373b21a71670b2d3118b2c637ee08
Sid-Straw <j80304xz@gmail.com>
2020-09-27 19:25:27 +0800

    build: 部署設定

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
 module.exports = {
   runtimeCompiler: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Cindy 情境式多益單字課 複習進度表',
+    },
+  },
   transpileDependencies: ['vuetify'],
   configureWebpack: {
     module: {
