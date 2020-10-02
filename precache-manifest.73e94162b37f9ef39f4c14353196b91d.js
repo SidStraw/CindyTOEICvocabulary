16e329aa02c79ff87a95b6572acfbf3d25862999
GitHub Action <action@github.com>
2020-10-02 07:04:41 +0000

    deploy to Github pages

---
+++ precache-manifest.73e94162b37f9ef39f4c14353196b91d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76107db0442fd5781003309813cd7f24",
+    "url": "/404.html"
+  },
+  {
+    "revision": "8e58efe7f0d59f28e3f7e546f97879cf",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "f961e461df1d11af8e2e",
+    "url": "/css/chunk-vendors.7c01d343.css"
+  },
+  {
+    "revision": "2424a39e8cedded14468",
+    "url": "/css/index.e2b6e39e.css"
+  },
+  {
+    "revision": "10810178d1c16cd09b091b93ecbf39f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1891c4bbacf9a7bc8976",
+    "url": "/js/about.c99aa5b4.js"
+  },
+  {
+    "revision": "f961e461df1d11af8e2e",
+    "url": "/js/chunk-vendors.d53b714b.js"
+  },
+  {
+    "revision": "2424a39e8cedded14468",
+    "url": "/js/index.cd99af6d.js"
+  },
+  {
+    "revision": "d7a9a81f854cf0f79f089563505f9b7b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
